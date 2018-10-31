5d3beb81f914179c4a6505e943c31225589357ae
towelie82 <31874531+towelie82@users.noreply.github.com>
2018-10-31 20:48:20 +0800

    Add files via upload

--- ICT286Assignment2/js/main.js
+++ ICT286Assignment2/js/main.js
@@ -14,6 +14,24 @@ var newPage = getPage(window.location.hash);
        var newPage = getPage(window.location.hash);
        render(newPage);
    });
+	$("#consoles").click(function(){
+        category("Game Console");
+    });
+	$("#phones").click(function(){
+        category("Phone");
+    });
+	$("#cameras").click(function(){
+        category("Camera");
+    });
+	$("#desktoplaptop").click(function(){
+        category("Desktop - Laptop");
+    });
+	$("#keyboardmice").click(function(){
+        category("Keyboard - Mouse");
+    });
+	$("#headphones").click(function(){
+        category("Headphones");
+    });
 
 });
  function render(newPage){
@@ -26,3 +44,32 @@ function getPage(hash){
    var i = pages.indexOf(window.location.hash);
    return i < 1 ? pages[0] : pages[i];
 }
+
+function search(str) 
+{
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4 && this.status == 200)
+		{
+			document.getElementById("results").innerHTML = this.responseText;
+		}
+	};
+	xmlhttp.open("GET", "search.php?query=" + str, true);
+	xmlhttp.send();
+}
+function category(str) 
+{
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4 && this.status == 200)
+		{
+			document.getElementById("results").innerHTML = this.responseText;
+		}
+	};
+	xmlhttp.open("GET", "category.php?query=" + str, true);
+	xmlhttp.send();
+}
+
+	
---
+++ ICT286Assignment2/search.php
@@ -0,0 +1,57 @@
+<?php
+
+// Connect to MySQL
+$host = "localhost";
+$user = "X30397745";
+$password = "X30397745";
+$dbname = "X30397745";
+$db = new mysqli($host, $user, $password, $dbname);
+if ($db->connect_error) 
+{
+     die("Connection Failed: " . $db->connect_error);
+}
+
+// Clean up the given query (delete leading and trailing whitespace)
+
+$search = $_REQUEST['query'];
+trim($search);
+$query = 'SELECT * FROM ' . $dbname . '.products WHERE Brand LIKE "%' . $search . '%" OR Make LIKE "%' . $search . '%" OR Model LIKE "%' . $search . '%" OR Details LIKE "%' . $search . '%";';
+// print "The query is: " . $query;
+// Execute the query
+
+$result = $db->query($query);
+if (!$result) {
+    print "Error - the query could not be executed";
+    $error = mysql_error();
+    print "<p>" . $error . "</p>";
+    exit;
+}
+
+// Display the results in a table
+
+print "<table><caption> <h2> Search Results </h2> </caption>";
+
+if ($result->num_rows > 0) 
+{
+    // output data of each row
+    while($row = $result->fetch_assoc()) 
+    {
+        print "<tr>";
+		print "<td colspan='2'><h3>" . $row["Brand"] . " " . $row["Make"] . " " . $row["Model"] . "</h3></td>";
+		print "</tr>";
+        print "<tr>";
+		print "<td><img src='" . $row["Picture"] . "' alt='Item Picture' / ></td>";
+        print "<td> Price: $" . $row["Price"] . "<br /><br /> Button Here </td>";
+        print "</tr>";
+        print "<tr>";
+        print "<td colspan='2'> Details: <br/><br/> " . $row["Details"] . "</td>";
+        print "</tr>";
+    }
+} 
+else 
+{
+    print "<tr><td>No results found</td></tr>";
+}
+
+print "</table>";
+?>
